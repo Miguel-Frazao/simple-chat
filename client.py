3158dfb3ca08ff7f2c75ba4973a9a64f910a54e9
Miguel Frazão <miguelmssfrazao@gmail.com>
2016-11-23 10:23:13 +0000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,73 @@
+import socket, sys, select, json
+
+class Client:
+	def __init__(self):
+		self.username = None
+
+	def connect(self, host, port):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			s.connect((host, port))
+		except Exception as err:
+			print('Unable to connect: {}'.format(err))
+			sys.exit()
+		print('Connecting to {} on port {}\n'.format(host, port))
+		self.begin_chat(s)
+
+	def begin_chat(self, client_sock):
+		while True:
+			socket_list = [sys.stdin, client_sock]
+			#print(socket_list)
+			ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
+			for sock in ready_to_read:
+				if sock == client_sock:
+					# incoming message from remote server, s
+					data = sock.recv(4096)
+					if not data:
+						print('\nDisconnected from chat server')
+						sys.exit()
+					else:
+						data = json.loads(data.decode('utf-8'))
+						color = '\033[1;32;40m'
+						if(data['from'] == 'SERVER'):
+							color = '\033[1;30;40m'
+						output = '\r{}[{}]: {}\033[0;37;40m \n'.format(color, data['from'], data['message'])
+						sys.stdout.write(output)
+						if(self.username is None):
+							sys.stdout.write('Your username?\n')
+							sys.stdout.flush()
+							continue
+						sys.stdout.write('\033[1;34;40m[ME]:\033[0;37;40m ')
+						sys.stdout.flush()
+				else:
+					#send message to server
+					msg = sys.stdin.readline()
+					if(msg.strip() != ''):
+						if(self.username is None):
+							self.username = msg
+						client_sock.send(msg.encode('utf-8'))
+					sys.stdout.write('\033[1;34;40m[Me]:\033[0;37;40m ')
+					sys.stdout.flush()
+
+
+client = Client()
+client.connect('', 9005) # adjust host/port
+
+
+	
+
+
+'''
+data = s.recv(1024).decode('utf-8')
+if data:
+	print(data)
+msg = input('show fibonacci of:')
+try:
+    int(msg)
+except ValueError:
+	print('formato errado')
+	continue
+s.send(msg.encode('ascii')) 
+data = s.recv(1024).decode('utf-8')
+print(data)
+'''
---
+++ server.py
@@ -0,0 +1,89 @@
+import threading, socket, select, json, queue
+
+class Chat_Server:
+	def __init__(self):
+		self.conns = set() # all connections
+		self.actives = set() # connections who provided an username
+		self.data = {} # all data we want to store
+		self.print_queue = queue.Queue()
+		t = threading.Thread(target=self.print_manager)
+		t.daemon = True
+		t.start()
+		del t
+
+	def print_manager(self):
+		while True:
+			msg = self.print_queue.get()
+			print(msg)
+			self.print_queue.task_done()
+
+	def run(self, addr, port):
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+			sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			sock.bind((addr, port))
+			sock.listen(5)
+			self.print_queue.put('Server started at {}:{}\n'.format(addr, port))
+			while True:
+				client, addr = sock.accept()
+				self.conns.add(client)
+				self.print_queue.put("New Connection: {}\nConnections {}: {}\n\n".format(addr, len(self.conns), self.conns))
+				t = threading.Thread(target=self.server_handler, args=(client,), name='t_{}'.format(self.client_name(client)))
+				t.start()
+
+	def client_name(self, client):
+		return ':'.join(str(i) for i in client.getpeername())
+
+	def client_close(self, client):
+		self.print_queue.put('\n[-] Client {} ({}) closed the conection'.format(client.getpeername(), self.data[self.client_name(client)]))
+		self.conns.remove(client)
+		del self.data[self.client_name(client)]
+		if client in self.actives:
+			self.actives.remove(client)
+		self.print_queue.put("[-] Connections ({}): {}\n[-] Actives ({}): {}\n\n".format(len(self.conns), self.conns, len(self.actives), self.actives))
+
+	def send_msg(self, msg, sender, to):
+		to_send = {'from': sender, 'message': msg}
+		to.send(json.dumps(to_send).encode('utf-8'))
+
+	def show_report(self, raw, req, client):
+		recipients = ', '.join(self.data[self.client_name(i)]['username'] for i in self.actives if i is not client)
+		report_msg = '[+] Thread: {}\n[+] Message sent from {} to: {}\n[+] Content: {} ({})'
+		report = report_msg.format(threading.currentThread().getName(), self.data[self.client_name(client)]['username'], recipients, raw, req)
+		end_output = '{}\n[+] Total data: {}\n\n'.format(report, self.data)
+		self.print_queue.put(end_output)
+
+	def send_to_all(self, msg, client, sender):
+		if(sender != 'SERVER'):
+			self.data[self.client_name(client)]['msgs_sent'].append(msg)
+		ready_to_read,ready_to_write,in_error = select.select(self.actives, self.actives,[],0)
+		for sock_write in ready_to_write:
+			if(sock_write is not client):
+				self.send_msg(msg, sender, sock_write)
+
+
+	def server_handler(self, client):
+		self.data[self.client_name(client)] = {'msgs_sent': [], 'username': None}
+		msg = 'welcome...{}.\nThere are this users connected: {}\n\n'
+		msg = msg.format(client.getpeername(), [i.getpeername() for i in self.conns])
+		self.send_msg(msg, 'SERVER', client)
+		while True:
+			try:
+				raw = client.recv(1024)
+				req = raw.decode('utf-8').strip()
+			except Exception as err:
+				break
+			if not req:
+				self.client_close(client)
+				break
+			if(self.data[self.client_name(client)]['username'] is None):
+				self.data[self.client_name(client)]['username'] = req
+				self.send_to_all('{} HAS JOINED THE CHAT...'.format(req), client, 'SERVER')
+				self.actives.add(client) # store all clients that gave an username
+				continue
+
+			self.send_to_all(req, client, self.data[self.client_name(client)]['username'])
+			self.show_report(raw, req, client)
+
+
+chat = Chat_Server()
+chat.run('', 9005) # adjust host/port
